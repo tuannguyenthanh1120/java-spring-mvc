b707500ad82ea12ccc3767acd9ae7668e73832a1
tuannguyenthanh1120 <tuannguyenthanh.work@gmail.com>
2025-08-12 20:26:19 +0700

    init project

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,21 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		// container
+		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : hoidanit.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
+
 	}
 
 }
